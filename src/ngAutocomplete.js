027d7199d54326ea6e5fff7e52c7bd55d5eec6b7
Andrey Tabachnik <fxuniverse@gmail.com>
2015-06-22 15:25:45 -0400

    added onEnter placeChanged

--- src/ngAutocomplete.js
+++ src/ngAutocomplete.js
@@ -145,6 +145,7 @@ function ngAutocomplete() {
                         })
 
                       });
+                      scope.onPlaceChanged();
                     }
                   }
                 );
